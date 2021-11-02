b09273d5164b05b16b4f4dd947e94ae1a1d6d30f
aurelien-adjimi <91850318+aurelien-adjimi@users.noreply.github.com>
2021-11-02 12:10:06 +0100

    job02 finish

---
+++ jour01/job02/index.php
@@ -0,0 +1,12 @@
+<?php
+//Hello LaPlateforme avec variable//
+$one = "Hello";
+$two = "LaPlateforme";
+$three = "!";
+
+echo $one." ".$two." ".$three;
+
+/* Hello LaPlateforme avec echo */
+echo "Hello LaPlateforme!";
+
+?>
