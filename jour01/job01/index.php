dce396678341143d46eb6024a76ae116fcc8e021
aurelien-adjimi <91850318+aurelien-adjimi@users.noreply.github.com>
2021-11-02 11:54:59 +0100

    job01 finish

---
+++ jour01/job01/index.php
@@ -0,0 +1,20 @@
+<?php
+$str = "LaPlateforme";
+$str2 = "Vive";
+$str3 = "!";
+
+echo $str2." ".$str." ".$str3;
+
+$val = 6;
+echo $val;
+
+$val = ($val2 = 4) + 6;
+echo $val;
+
+$myBool = true;
+echo $myBool;
+
+$myBool = false;
+echo $myBool;
+
+?>
