9355b6e9db2493b8f541981b41c845e18990be77
aurelien-adjimi <91850318+aurelien-adjimi@users.noreply.github.com>
2021-11-03 11:11:05 +0100

    J2 Job1 finish

---
+++ jour02/job01/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+for ($j = 0; $j <= 1337; $j++) {  
+    if ($j == 42) {
+        echo "<b> <u>".$j."</b></u>";
+        echo "<br/>";
+    }
+    else {
+    echo $j;
+    echo "<br/>";
+    }
+}
+?>
