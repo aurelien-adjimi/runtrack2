ea1fcd4fc0b8e57c8b163a030949355229c3c8f1
aurelien-adjimi <91850318+aurelien-adjimi@users.noreply.github.com>
2021-11-03 17:39:10 +0100

    job04 modifié et job05 finish

--- jour02/job04/index.php
+++ jour02/job04/index.php
@@ -1,14 +1,18 @@
 <?php
 
-for ($i = 0; $i <= 100; $i++) {
+for ($i = 1; $i <= 100; $i++) {
     if (!($i % 3) && !($i % 5)) {
         echo "FizzBuzz";
-    } elseif (!($i %5)){
+        echo "<br/>";
+    } elseif (!($i % 5)){
         echo "Buzz";
+        echo "<br/>";
     } elseif (!($i % 3)) {
         echo "Fizz";
-    }
+        echo "<br/>";
+    } else {
     echo $i;
-    echo "<br/>";
+    echo "<br/>";  
+    } 
 }
 ?>
