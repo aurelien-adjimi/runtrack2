8f1d376512587c7f97034ef88535e5baed5da8a2
aurelien-adjimi <91850318+aurelien-adjimi@users.noreply.github.com>
2021-11-09 11:21:07 +0100

    job02 finish

--- jour07/job02/index.php
+++ jour07/job02/index.php
@@ -1,8 +1,14 @@
 <?php
 
-setcookie('nbvisites', '0', time()+ 3600);
+setcookie('nbvisites', 0, time()+ 3600);
+
+if(isset($_POST["Reset"])) {
+    unset ($_COOKIE['nbvisites']);   
+}
+
 if(isset($_COOKIE['nbvisites'])) {
-    $_COOKIE['nbvisites']++;
+    setcookie ('nbvisites', $_COOKIE['nbvisites']+1 , time()+ 3600);
+
 }
 
 else {
@@ -11,3 +17,18 @@ else {
 echo $_COOKIE['nbvisites'];
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="#" method="post">
+        <input type="submit" value="Reset" name="Reset">
+    </form>
+</body>
+</html>
